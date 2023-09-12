afbc7398a996a94d7580d975cb6aeea811d9f609
NTSonny <nsonnenberg@enlitehome.com>
2023-09-12 08:14:47 -0600

    Finalized making footer mobile responsive.

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,19 +5,21 @@ export default function Footer(): JSX.Element {
   return (
     <div className="bottom-0 h-32 w-full bg-primary-variant text-on-primary">
       <div className="flex flex-row h-full items-center">
-        <div className="basis-1/3 text-2xl ml-40">&copy; 2023</div>
-        <div className="basis-2/3 flex flex-row justify-evenly mr-20">
+        <div className="tablet:basis-1/3 tablet:text-2xl tablet:ml-40 phone:text-lg phone:ml-4">
+          &copy; 2023
+        </div>
+        <div className="basis-2/3 grow flex flex-row justify-evenly tablet:mr-20 phone:mr-4">
           <div>
-            <FiLinkedin className="w-11 h-11" />
+            <FiLinkedin className="tablet:w-11 tablet:h-11 phone:w-6 phone:h-6" />
           </div>
           <div>
-            <FiInstagram className="w-11 h-11" />
+            <FiInstagram className="tablet:w-11 tablet:h-11 phone:w-6 phone:h-6" />
           </div>
           <div>
-            <FiTwitter className="w-11 h-11" />
+            <FiTwitter className="tablet:w-11 tablet:h-11 phone:w-6 phone:h-6" />
           </div>
           <div>
-            <FiFacebook className="w-11 h-11" />
+            <FiFacebook className="tablet:w-11 tablet:h-11 phone:w-6 phone:h-6" />
           </div>
         </div>
       </div>
