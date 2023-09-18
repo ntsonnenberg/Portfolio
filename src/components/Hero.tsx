f5b962244e02c910576f9f618731ed6fade91b20
NTSonny <nsonnenberg@enlitehome.com>
2023-09-18 08:35:37 -0600

    Finished responsivenss of Hero section. Working on animating the images on Hero section.

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -3,29 +3,35 @@ import Button from "./Button";
 
 export default function Hero(): JSX.Element {
   return (
-    <div className="h-screen bg-primary-variant/80 text-on-primary flex flex-row phone:flex-col tablet:flex-col laptop:flex-row">
+    <div className="phone:h-[150vh] tablet:h-screen bg-primary-variant/80 text-on-primary flex flex-row phone:flex-col tablet:flex-col laptop:flex-row">
       <div className="basis-3/5 relative">
         <img
           src="/landing-content.svg"
-          className="w-auto h-60 absolute z-20 mt-32"
+          className="w-auto h-60 absolute z-20 animate-fade-in-down mt-32 phone:top-12 tablet:top-0"
         />
-        <img src="/hero.svg" className="w-auto h-96 z-10 absolute mt-44" />
+        <img
+          src="/hero.svg"
+          className="w-auto h-96 z-10 absolute mt-44 animate-fade-in-down"
+        />
         <img
           src="/yearly-metrics.svg"
-          className="w-auto h-52 z-0 absolute bottom-12 ml-40"
+          className="w-auto h-52 z-0 absolute ml-40 animate-fade-in-down phone:bottom-32 phone:right-10 tablet:bottom-0 tablet:left-2 laptop:bottom-12 desktop:bottom-60"
         />
         <img
           src="/products.svg"
-          className="w-auto h-72 absolute z-20 bottom-20 mb-6 right-36 mr-4"
+          className="absolute z-20 mb-6 mr-4 animate-fade-in-down phone:h-52 phone:w-auto phone:bottom-4 phone:right-0 tablet:w-auto tablet:h-72 tablet:bottom-10 tablet:right-14 laptop:bottom-20 laptop:right-36 desktop:bottom-72 desktop:right-80"
         />
-        <img src="/cart.svg" className="w-auto h-52 absolute z-20 bottom-16" />
+        <img
+          src="/cart.svg"
+          className="w-auto h-52 absolute z-20 animate-fade-in-down phone:bottom-10 tablet:bottom-16 desktop:bottom-60"
+        />
         <img
           src="/ecommerce-landing.svg"
-          className="w-auto h-64 absolute z-30 right-56 top-20"
+          className="w-auto h-64 absolute z-30 top-20 animate-fade-in-down phone:right-20 tablet:right-36 laptop:right-56 desktop:right-96"
         />
       </div>
-      <div className="basis-2/5 flex flex-col my-20 mx-10 phone:mx-6 tablet:mx-10 gap-10">
-        <h1 className="text-6xl phone:text-5xl font-bold">
+      <div className="basis-2/5 flex flex-col my-20 mx-10 gap-10 phone:mx-6 tablet:mx-10">
+        <h1 className="text-6xl font-bold phone:text-5xl">
           Professional Web Development Services
         </h1>
         <h2 className="text-2xl">
--- tailwind.config.js
+++ tailwind.config.js
@@ -29,6 +29,21 @@ module.exports = {
       fontFamily: {
         poppins: ["Poppins", "sans-serif"],
       },
+      keyframes: {
+        "fade-in-down": {
+          "0%": {
+            opacity: "0",
+            transform: "translateY(-20px)",
+          },
+          "100%": {
+            opacity: "1",
+            transform: "translateY(0)",
+          },
+        },
+      },
+      animation: {
+        "fade-in-down": "fade-in-down 1s ease-out",
+      },
     },
   },
   plugins: [],
