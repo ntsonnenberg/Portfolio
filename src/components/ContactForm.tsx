cbca5e02fa19bb26413a34a9fbf353139303891a
NTSonny <nsonnenberg@enlitehome.com>
2023-09-20 08:35:55 -0600

    Finalized contact form and made i mobile responsive.

--- src/components/ContactForm.tsx
+++ src/components/ContactForm.tsx
@@ -3,12 +3,28 @@ import Button from "./Button";
 import Input from "./Input";
 
 export default function ContactUs(): JSX.Element {
+  const isLaptop = window.innerWidth >= 1536;
+  const isTablet = window.innerWidth < 1536 && window.innerWidth >= 664;
+  const isPhone = window.innerWidth < 664;
+
   return (
     <div className="bg-primary-variant text-on-primary flex flex-col text-center items-center py-10 gap-5">
-      <h1 className="font-bold phone:text-2xl">Contact Us to Learn More</h1>
+      <h1 className="font-bold phone:text-2xl tablet:text-5xl tablet:mb-10 laptop:text-7xl laptop:mb-16">
+        Contact Us to Learn More
+      </h1>
       <form>
-        <Input label="Email" placeholder="Enter email..." />
-        <Button light filled className="mt-5 w-full rounded-sm">
+        <Input
+          label="Email"
+          placeholder="Enter email..."
+          small={isPhone}
+          medium={isTablet}
+          large={isLaptop}
+        />
+        <Button
+          light
+          filled
+          className="w-full rounded-sm phone:mt-5 phone:h-auto tablet:mt-8 laptop:h-10"
+        >
           Submit
         </Button>
       </form>
--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,19 +1,42 @@
 import React from "react";
-
+import classNames from "classnames";
 interface Props {
   label: string;
   placeholder?: string;
+  className?: string;
+  small?: boolean;
+  medium?: boolean;
+  large?: boolean;
 }
 
-export default function Input({ label, placeholder }: Props): JSX.Element {
+export default function Input({
+  label,
+  placeholder,
+  small,
+  medium,
+  large,
+  className,
+}: Props): JSX.Element {
+  const labelClasses = classNames({
+    "text-md": small,
+    "text-xl": medium,
+    "text-3xl": large,
+  });
+
+  const inputClasses = classNames(
+    "bg-transparent border-b-2 focus:outline-0 focus:border-2 focus:rounded-sm",
+    {
+      "w-60 h-auto": small,
+      "w-80 h-10": medium,
+      "w-96 h-12": large,
+    },
+    className
+  );
+
   return (
     <div className="flex flex-col items-start gap-1">
-      <label>{label}</label>
-      <input
-        type="text"
-        placeholder={placeholder}
-        className="bg-transparent border-b-2"
-      />
+      <label className={labelClasses}>{label}</label>
+      <input type="text" placeholder={placeholder} className={inputClasses} />
     </div>
   );
 }
