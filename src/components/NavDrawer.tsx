719e7f235bd5c5f7847fb2c624288fd5edf26cd8
NTSonny <nsonnenberg@enlitehome.com>
2023-10-09 08:26:29 -0600

    Finalized portfolio cards with animations. Also modified layout of cards on Portfolio page.

--- src/components/NavDrawer.tsx
+++ src/components/NavDrawer.tsx
@@ -24,7 +24,7 @@ export default function NavDrawer({ closeDrawer }: Props): JSX.Element {
         className="fixed inset-0 bg-background opacity-80"
         onClick={closeDrawer}
       ></div>
-      <div className="fixed inset-y-0 right-0 w-1/3 bg-primary-variant text-on-background drop-shadow-drawer flex flex-col">
+      <div className="fixed inset-y-0 right-0 bg-primary-variant text-on-background drop-shadow-drawer flex flex-col phone:w-4/5 tablet:w-1/2">
         <FiX
           className="w-10 h-10 self-end m-6 cursor-pointer"
           onClick={closeDrawer}
