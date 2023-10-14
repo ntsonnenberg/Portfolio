938f66719ee39e62a9a4facc9472bdbb48015577
NTSonny <nsonnenberg@enlitehome.com>
2023-10-14 09:43:18 -0600

    Modified parallax.

--- src/components/ParallaxFeature.tsx
+++ src/components/ParallaxFeature.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function ParallaxFeature(): JSX.Element {
   return (
-    <div className="min-h-screen flex justify-center text-center items-center bg-code bg-cover bg-no-repeat bg-center bg-fixed">
+    <div className="min-h-screen flex justify-center text-center items-center bg-code bg-no-repeat bg-center bg-fixed phone:bg-[length:500px_600px] tablet:bg-[length:850px_900px] laptop:bg-cover">
       <h1 className="text-white font-bold phone:text-5xl tablet:text-7xl">
         Web Solutions that Work
       </h1>
