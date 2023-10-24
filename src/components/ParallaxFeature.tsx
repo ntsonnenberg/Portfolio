af1ce481c98b9b4bbfe1c84f40a92b432b5e4a3d
NTSonny <nsonnenberg@enlitehome.com>
2023-10-24 08:05:06 -0600

    Removed parallax scroll for mobile view.

--- src/components/ParallaxFeature.tsx
+++ src/components/ParallaxFeature.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function ParallaxFeature(): JSX.Element {
   return (
-    <div className="min-h-screen flex justify-center text-center items-center bg-code bg-no-repeat bg-center bg-fixed bg-cover">
+    <div className="min-h-screen flex justify-center text-center items-center bg-code bg-no-repeat bg-center bg-cover laptop:bg-fixed">
       <h1 className="text-white font-bold phone:text-5xl tablet:text-7xl">
         Web Solutions that Work
       </h1>
