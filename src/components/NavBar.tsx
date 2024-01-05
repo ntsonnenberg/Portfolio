ab98c36c152c83a7bc2426bfed4a8076ee6124ef
NTSonny <nsonnenberg@enlitehome.com>
2024-01-05 08:17:51 -0700

    Made changes to color scheme and nav bar size.

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -35,7 +35,7 @@ export default function NavBar({ openDrawer }: Props): JSX.Element {
     <div
       className={`transition-all duration-500 ${
         showNav ? "opacity-100" : "opacity-0"
-      } h-40 w-full bg-primary-variant/80 text-on-primary backdrop-blur-lg drop-shadow-2xl fixed top-0 z-50`}
+      } h-28 w-full bg-primary-variant/80 text-on-primary backdrop-blur-lg drop-shadow-2xl fixed top-0 z-50`}
     >
       <div className="flex flex-row justify-around h-full items-center align-center">
         <Link to="/" className="cursor-pointer phone:text-2xl tablet:text-4xl">
@@ -43,29 +43,29 @@ export default function NavBar({ openDrawer }: Props): JSX.Element {
             src="../images/bespoke-white-no-bg.png"
             alt="Bespoke Dev Solutions"
             placeholder="blurred"
-            className="phone:w-48 phone:h-auto tablet:w-auto"
+            className="phone:w-48 phone:h-auto tablet:w-40 tablet:h-auto"
           />
         </Link>
         <FiMenu
           className="cursor-pointer phone:h-6 phone:w-6 tablet:h-10 tablet:w-10 laptop:hidden"
           onClick={openDrawer}
         />
-        <div className="hidden laptop:flex flex-row gap-24">
+        <div className="hidden laptop:flex flex-row gap-24 text-xl cursor-pointer">
           <Link
             to="/projects"
-            className="text-2xl cursor-pointer transition-transform duration-100 hover:-translate-y-2 hover:ease-in hover:text-secondary hover:font-bold"
+            className="transition-transform duration-100 hover:-translate-y-2 hover:ease-in hover:text-secondary hover:font-bold"
           >
             Projects
           </Link>
           <Link
             to="/aboutme"
-            className="text-2xl cursor-pointer transition-transform duration-100 hover:-translate-y-2 hover:ease-in hover:text-secondary hover:font-bold"
+            className="transition-transform duration-100 hover:-translate-y-2 hover:ease-in hover:text-secondary hover:font-bold"
           >
             About Me
           </Link>
           <Link
             to="/contact"
-            className="text-2xl cursor-pointer transition-tranform duration-100 hover:-translate-y-2 hover:ease-in hover:text-secondary hover:font-bold"
+            className="transition-transform duration-100 hover:-translate-y-2 hover:ease-in hover:text-secondary hover:font-bold"
           >
             Contact
           </Link>
