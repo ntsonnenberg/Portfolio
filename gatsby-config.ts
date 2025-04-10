5a396c0856ea7e0c2c1b01260b93b7d839c4c0b6
Nathan Sonnenberg <ntsonnenberg@gmail.com>
2025-04-10 15:29:50 -0600

    fixed gatsby confid

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -40,7 +40,6 @@ const config: GatsbyConfig = {
   plugins: [
     "gatsby-plugin-postcss",
     "gatsby-plugin-image",
-    "gatsby-plugin-sitemap",
     "gatsby-plugin-sharp",
     "gatsby-transformer-sharp",
     "gatsby-plugin-smoothscroll",
