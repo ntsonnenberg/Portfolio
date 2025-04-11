6a69e08759b1b156e18a70b39e006ed150ebe2bb
Nathan Sonnenberg <ntsonnenberg@gmail.com>
2025-04-11 13:53:54 -0600

    Updated UI on contact form inputs.

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -24,22 +24,14 @@ export default function Input({
       >
         {label} {required && <p className="inline text-sm align-top">*</p>}
       </label>
-      <div
-        className={`relative inline-block bg-transparent w-full phone:py-1 phone:h-6 tablet:h-8 laptop:py-0 laptop:h-8 ${className}`}
-      >
-        <input
-          id={name}
-          name={name}
-          type={type}
-          placeholder={placeholder}
-          required={required}
-          className="peer w-full h-full decoration-none bg-transparent border-b-2 pl-2 focus:border-0 focus:outline-0"
-        />
-        <span className="ease absolute left-0 top-0 h-0 w-0 border-t-2 border-white transition-all duration-200 peer-focus:w-full"></span>
-        <span className="ease absolute right-0 top-0 h-0 w-0 border-r-2 border-white transition-all duration-200 peer-focus:h-full"></span>
-        <span className="ease absolute bottom-0 right-0 h-0 w-0 border-b-2 border-white transition-all duration-200 peer-focus:w-full"></span>
-        <span className="ease absolute bottom-0 left-0 h-0 w-0 border-l-2 border-white transition-all duration-200 peer-focus:h-full"></span>
-      </div>
+      <input
+        id={name}
+        name={name}
+        type={type}
+        placeholder={placeholder}
+        required={required}
+        className="w-full p-2 decoration-none bg-[#050A30] rounded-md focus:ring-1 focus:ring-primary focus:outline-0"
+      />
     </div>
   );
 }
--- src/components/Textarea.tsx
+++ src/components/Textarea.tsx
@@ -24,19 +24,13 @@ export default function Textarea({
       >
         {label} {required && <p className="inline text-sm align-top">*</p>}
       </label>
-      <div className="relative inline-block w-full bg-transparent text-on-background">
-        <textarea
-          id={name}
-          name={name}
-          required={required}
-          placeholder={placeholder}
-          className={`peer bg-transparent text-on-background h-20 w-full border-b-2 pl-2 pt-2 focus:border-0 focus:outline-0 ${className}`}
-        />
-        <span className="ease absolute left-0 top-0 h-0 w-0 border-t-2 border-white transition-all duration-200 peer-focus:w-full"></span>
-        <span className="ease absolute right-0 top-0 h-0 w-0 border-r-2 border-white transition-all duration-200 peer-focus:h-full"></span>
-        <span className="ease absolute bottom-0 right-0 h-0 w-0 border-b-2 border-white transition-all duration-200 peer-focus:w-full"></span>
-        <span className="ease absolute bottom-0 left-0 h-0 w-0 border-l-2 border-white transition-all duration-200 peer-focus:h-full"></span>
-      </div>
+      <textarea
+        id={name}
+        name={name}
+        required={required}
+        placeholder={placeholder}
+        className="w-full p-2 decoration-none bg-[#050A30] rounded-md focus:ring-1 focus:ring-primary focus:outline-0"
+      />
     </div>
   );
 }
